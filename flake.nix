befd9a6af3c4bd1815768f4dba4ba28f4e383916
Nick Cao <nickcao@nichi.co>
2022-10-16 13:45:55 +0800

    rcore-fs-fuse: fix build with fuse

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
         {
           devShells.default = mkShell {
             nativeBuildInputs = [ rustc cargo rustfmt rust-analyzer pkg-config ];
-            buildInputs = [ fuse3 ];
+            buildInputs = [ fuse ];
           };
         }
       );
