de411683e3e2b6320e1780e34ed35aac489d03b5
Nick Cao <nickcao@nichi.co>
2022-11-01 12:18:57 +0800

    impl store for hashmap

--- rcore-fs-dfs/src/store.rs
+++ rcore-fs-dfs/src/store.rs
@@ -1,5 +1,21 @@
+use std::collections::HashMap;
+
 pub trait Store {
     fn put<K: AsRef<[u8]>, V: AsRef<[u8]>>(&mut self, key: K, value: V) -> std::io::Result<()>;
     fn get<K: AsRef<[u8]>>(&mut self, key: K) -> std::io::Result<Option<Vec<u8>>>;
     fn delete<K: AsRef<[u8]>>(&mut self, key: K) -> std::io::Result<()>;
+}
+
+impl Store for HashMap<Vec<u8>, Vec<u8>> {
+    fn put<K: AsRef<[u8]>, V: AsRef<[u8]>>(&mut self, key: K, value: V) -> std::io::Result<()> {
+        self.insert(key.as_ref().to_vec(), value.as_ref().to_vec());
+        Ok(())
+    }
+    fn get<K: AsRef<[u8]>>(&mut self, key: K) -> std::io::Result<Option<Vec<u8>>> {
+        Ok(HashMap::get(self, key.as_ref()).map(|value| value.clone()))
+    }
+    fn delete<K: AsRef<[u8]>>(&mut self, key: K) -> std::io::Result<()> {
+        self.remove(key.as_ref());
+        Ok(())
+    }
 }
