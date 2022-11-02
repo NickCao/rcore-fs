7766fb54f4c2285e1cd0e3092aab461f574257d3
Nick Cao <nickcao@nichi.co>
2022-11-02 16:08:35 +0800

    restore fuse mount

--- rcore-fs-dfs/src/main.rs
+++ rcore-fs-dfs/src/main.rs
@@ -1,9 +1,8 @@
-use dfs::transport::{LoopbackTransport, Transport};
-use rcore_fs::vfs::FileSystem;
+use dfs::transport::LoopbackTransport;
 use rcore_fs_dfs as dfs;
 use rcore_fs_fuse::fuse::VfsFuse;
 use rcore_fs_ramfs as ramfs;
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::Arc};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -14,10 +13,9 @@ struct Opt {
 }
 
 fn main() {
-    /*
     let opt = Opt::from_args();
     let store = ramfs::RamFS::new();
-    let fs = dfs::DFS::new(0, 0, store);
+    let trans = LoopbackTransport::new(0, 1, 3000).unwrap();
+    let fs = dfs::DFS::new(Arc::new(trans), store);
     fuse::mount(VfsFuse::new(fs), &opt.dir, &[]).expect("failed to mount fs");
-    */
 }
