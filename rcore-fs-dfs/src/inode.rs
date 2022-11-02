c7828a19ec8b95b3ba6cf64040b7de55e3e71cd0
Nick Cao <nickcao@nichi.co>
2022-11-02 16:26:44 +0800

    fix root inode

--- rcore-fs-dfs/src/inode.rs
+++ rcore-fs-dfs/src/inode.rs
@@ -1,8 +1,12 @@
-use crate::transport::Transport;
+use crate::transport::{self, Transport};
 use core::any::Any;
 use rcore_fs::vfs::*;
+use serde::{Deserialize, Serialize};
 use std::string::String;
 use std::sync::Arc;
+use std::usize;
+
+const MAX_INODE_SIZE: usize = 4096;
 
 pub struct DINode {
     trans: Arc<dyn Transport>,
@@ -10,8 +14,39 @@ pub struct DINode {
     bid: u64,
 }
 
+#[derive(Serialize, Deserialize)]
+pub enum DFileType {
+    File,
+    Dir,
+    SymLink,
+    CharDevice,
+    BlockDevice,
+    NamedPipe,
+    Socket,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DMetadata {
+    pub type_: DFileType,
+    pub mode: u16,
+}
+
 impl DINode {
     pub fn new(trans: Arc<dyn Transport>, nid: u64, bid: u64) -> Arc<Self> {
+        let mut buf = vec![0u8; MAX_INODE_SIZE];
+        if trans.get(nid, bid, &mut buf).is_err() {
+            trans
+                .set(
+                    nid,
+                    bid,
+                    &bincode::serialize(&DMetadata {
+                        mode: 0o0755,
+                        type_: DFileType::Dir,
+                    })
+                    .unwrap(),
+                )
+                .unwrap();
+        }
         Arc::new(Self { trans, nid, bid })
     }
 }
@@ -50,7 +85,7 @@ impl rcore_fs::vfs::INode for DINode {
     }
 
     fn get_entry(&self, id: usize) -> Result<String> {
-        unimplemented!()
+        Err(FsError::EntryNotFound)
     }
 
     fn poll(&self) -> Result<PollStatus> {
@@ -70,7 +105,28 @@ impl rcore_fs::vfs::INode for DINode {
     */
 
     fn metadata(&self) -> Result<Metadata> {
-        unimplemented!()
+        let mut buf = vec![0u8; MAX_INODE_SIZE];
+        let n = self.trans.get(self.nid, self.bid, &mut buf).unwrap();
+        let meta: DMetadata = bincode::deserialize(&buf[..n]).unwrap();
+        Ok(Metadata {
+            dev: 0,
+            inode: 0,
+            size: 0,
+            blk_size: 0,
+            blocks: 0,
+            atime: Timespec { sec: 0, nsec: 0 },
+            mtime: Timespec { sec: 0, nsec: 0 },
+            ctime: Timespec { sec: 0, nsec: 0 },
+            type_: match meta.type_ {
+                DFileType::Dir => FileType::Dir,
+                _ => unreachable!(),
+            },
+            mode: meta.mode,
+            nlinks: 1,
+            uid: 0,
+            gid: 0,
+            rdev: 0,
+        })
     }
 
     fn set_metadata(&self, metadata: &Metadata) -> Result<()> {
